ce17bd3c3d793ff6843fced4287f61c65abdc45f
sarosh4271 <74011881+sarosh4271@users.noreply.github.com>
2022-01-04 11:28:07 +0500

    Initial Commit

---
+++ Hacker-News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker-News
+//
+//  Created by Sarosh Tahir on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
